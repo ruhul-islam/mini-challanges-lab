0af4499aa5a203dbc003f61b18a5de6325cf014f
ruhul-islam <islamruhul48092@gmail.com>
2022-07-06 10:59:05 -0400

    updated after review

--- script.js
+++ script.js
@@ -2,39 +2,54 @@
 
 let sum = 0;
 const totalParagraph = document.querySelector(".total");
-const cola = document.querySelector(".cola");
-const peanuts = document.querySelector(".peanuts");
-const chocolate = document.querySelector(".chocolate");
-const gummies = document.querySelector(".gummies");
-const changeForm = document.querySelector(".change-form");
+// console.dir(totalParagraph);  --- to check
+const moneyForm = document.querySelector(".money-form");
 const coinContainer = document.querySelector(".coin-container");
+const bulb = document.querySelector(".bulb");
+const on = document.querySelector(".onBtn");
+const off = document.querySelector(".offBtn");
+const toggle = document.querySelector(".toggleBtn");
+const end = document.querySelector(".endBtn");
 
-cola.addEventListener("click", () => {
-  sum += 2;
-  totalParagraph.textContent = `Total: $${sum}.00`;
-});
-
-peanuts.addEventListener("click", () => {
-  sum += 3;
-  totalParagraph.textContent = `Total: $${sum}.00`;
-});
-chocolate.addEventListener("click", () => {
-  sum += 4;
-  totalParagraph.textContent = `Total: $${sum}.00`;
-});
-gummies.addEventListener("click", () => {
-  sum += 6;
-  totalParagraph.textContent = `Total: $${sum}.00`;
+const buttons = document.querySelectorAll(".vending button");
+buttons.forEach((item) => {
+  item.addEventListener("click", () => {
+    sum += parseInt(item.getAttribute("data-cost"));
+    totalParagraph.textContent = `Total $${sum}.00`;
+  });
 });
 
-changeForm.addEventListener("submit", (e) => {
+// const colaBtn = document.querySelector(".cola");
+// const peanutsBtn = document.querySelector(".peanuts");
+// const ChocolatBtn = document.querySelector(".chocolate");
+// const gummiesBtn = document.querySelector(".gummies");
+
+// colaBtn.addEventListener("click", () => {
+//   sum += 2;
+//   totalParagraph.textContent = `Total $${sum}.00`;
+// });
+// peanutsBtn.addEventListener("click", () => {
+//   sum += 3;
+//   totalParagraph.textContent = `Total $${sum}.00`;
+// });
+// ChocolatBtn.addEventListener("click", () => {
+//   sum += 4;
+//   totalParagraph.textContent = `Total $${sum}.00`;
+// });
+// gummiesBtn.addEventListener("click", () => {
+//   sum += 6;
+//   totalParagraph.textContent = `Total $${sum}.00`;
+// });
+
+////////////////////////////////////////////// 2 ///////////////////////////////////////////////
+moneyForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const amount = document.querySelector("#amount").value;
   const type = document.querySelector("#type").value;
   for (let i = 0; i < amount; i++) {
     const newCoin = document.createElement("li");
-    newCoin.classList.add("coin", type);
     newCoin.textContent = type;
+    newCoin.classList.add("coin", type);
     coinContainer.append(newCoin);
     newCoin.addEventListener("click", () => {
       newCoin.remove();
@@ -42,35 +57,21 @@ changeForm.addEventListener("submit", (e) => {
   }
 });
 
-/////////////////////3//////////////////
-
-const lightbulb = document.querySelector(".lightbulb");
-const turnOn = document.querySelector(".on");
-const turnOff = document.querySelector(".off");
-const toggleLight = document.querySelector(".toggle");
-const powerOutage = document.querySelector(".end");
-
-turnOn.addEventListener("click", () => {
-  lightbulb.style.backgroundColor = "yellow";
+//////////////////////////////////// 3///////////////////////////////////////////////
+on.addEventListener("click", () => {
+  bulb.classList.add("on");
+});
+off.addEventListener("click", () => {
+  bulb.classList.remove("on");
+});
+toggle.addEventListener("click", () => {
+  bulb.classList.toggle("on");
 });
 
-turnOff.addEventListener("click", () => {
-  lightbulb.style.backgroundColor = "darkgrey";
-});
-
-toggleLight.addEventListener("click", () => {
-  const backgroundColor = lightbulb.style.backgroundColor;
-  if (backgroundColor === "darkgrey") {
-    lightbulb.style.backgroundColor = "yellow";
-  } else {
-    lightbulb.style.backgroundColor = "darkgrey";
-  }
-});
-
-powerOutage.addEventListener("click", () => {
-  lightbulb.remove();
-  turnOn.disabled = true;
-  turnOff.disabled = true;
-  toggleLight.disabled = true;
-  powerOutage.disabled = true;
+end.addEventListener("click", () => {
+  bulb.remove();
+  on.disabled = true;
+  off.disabled = true;
+  toggle.disabled = true;
+  end.disabled = true;
 });
